9cdb7be9953e67c84d3efbc33311bb2cad7babce
seffax098 <dkutilkin@gmail.com>
2026-03-13 18:45:02 +0300

    lint warning

--- src/app/products/[id]/ProductPageClient.tsx
+++ src/app/products/[id]/ProductPageClient.tsx
@@ -3,35 +3,20 @@ import Back from './components/Back';
 import ProductCard from './components/ProductCard';
 import styles from './ProductPage.module.scss';
 import RelatedItems from './components/RelatedItems';
-import { useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 import SkeletonCard from './components/SkeletonCard';
 import ProductStore from '@shared/stores/product/ProductStore';
 import { observer } from 'mobx-react-lite';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 import { useLocalStore } from '@/shared/utils/useLocalStore';
 import { Product } from '@/shared/types/product';
-import { useAuthStore, useCartStore } from '@/shared/stores/root/hooks';
 
 const ProductPage = ({ product, related }: { product: Product; related: Product[] }) => {
-  const authStore = useAuthStore();
-  const cartStore = useCartStore();
-  const router = useRouter()
   const productStore = useLocalStore(() => new ProductStore())
-  const initialized = authStore.initialized;
-  const isAuth = authStore.isAuth;
 
   useEffect(() => {
     productStore.hydrateFromSSR({ product, relatedItems: related })
   }, [productStore, product, related])
-
-
-  const handleClick = useCallback((productId: number) => {
-    if (!initialized) return;
-
-    if (isAuth) cartStore.plus(productId);
-    else router.push("/authorization");
-  }, [initialized, isAuth, cartStore, router]);
 
   return (
     <div className={`${styles.productPage}`}>
